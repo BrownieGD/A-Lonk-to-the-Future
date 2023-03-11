ce6f3f5bae7da3249f809e5a0ac8f24b2ea5b9a9
Kaaaatze <127616951+Kaaaatze@users.noreply.github.com>
2023-03-11 19:10:09 +0100

    srh
    aRWSG

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from enemy import Enemy
 from sys import exit
 from Health import Health
+from Character import Character
 pygame.init()
 screen= pygame.display.set_mode((1000,600))
 pygame.display.set_caption('Lonk to the Future')
@@ -10,12 +11,9 @@ clock = pygame.time.Clock()
 test_surface = pygame.Surface((1000,600))
 test_surface.fill('cyan')
 
-Lonk = pygame.Surface((50,50)).convert_alpha()
-Lonk.fill('blue')
-Lonk_x_pos = 475
-Lonk_y_pos = 275
-vel = 5
+
 #Objekte
+Lonk = Character()
 enemy01 = Enemy(220,220,69,69,2)
 health_bar = Health(5)
 
@@ -26,39 +24,12 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    keys = pygame.key.get_pressed()
-    #character logik
-    if keys[pygame.K_w] and keys[pygame.K_a] and Lonk_y_pos>0 and Lonk_x_pos>0:
-        Lonk_y_pos -= (vel/1.5)
-        Lonk_x_pos -= (vel/1.5)
-
-    elif keys[pygame.K_w] and keys[pygame.K_d] and Lonk_x_pos<950 and Lonk_y_pos>0:
-        Lonk_y_pos -= (vel/1.5)
-        Lonk_x_pos += (vel/1.5)
-
-    elif keys[pygame.K_d] and keys[pygame.K_s] and Lonk_x_pos<950 and Lonk_y_pos<550:
-        Lonk_y_pos += (vel/1.5)
-        Lonk_x_pos += (vel/1.5)
-
-    elif keys[pygame.K_a] and keys[pygame.K_s] and Lonk_x_pos>0 and Lonk_y_pos<550 :
-        Lonk_y_pos += (vel/1.5)
-        Lonk_x_pos -= (vel/1.5)
-    elif keys[pygame.K_w] and Lonk_y_pos>0:
-        Lonk_y_pos -= vel
-
-    elif keys[pygame.K_s] and Lonk_y_pos<550:
-        Lonk_y_pos += vel
-
-    elif keys[pygame.K_a] and Lonk_x_pos>0:
-        Lonk_x_pos -= vel
-
-    elif keys[pygame.K_d] and Lonk_x_pos<950:
-        Lonk_x_pos += vel
+    
     #Zeichnen
     test_surface.fill((0, 0, 0))
-    enemy01.bewegung(Lonk_x_pos,Lonk_y_pos)
+    enemy01.bewegung(Lonk.Lonk_x_pos,Lonk.Lonk_y_pos)
+    
     screen.blit(test_surface,(0,0))
-    screen.blit(Lonk,(Lonk_x_pos,Lonk_y_pos))
     health_bar.render_hp()
 
 
